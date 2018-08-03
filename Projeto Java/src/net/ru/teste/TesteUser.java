c6878f8097cd82fd7419e05773def0a109b6e9d7
Wilder Santos Carvalho <wcarvalho98@gmail.com>
2018-08-03 19:48:57 -0300

    Revert "wtf"
    This reverts commit 2869b59d4d839210b68c9877a557c3154b4e9cba.

--- Projeto Java/src/net/ru/teste/TesteUser.java
+++ Projeto Java/src/net/ru/teste/TesteUser.java
@@ -4,23 +4,37 @@ import java.util.Calendar;
 
 import net.ru.negocio.beans.Almoco;
 import net.ru.negocio.beans.Usuario;
+import net.ru.negocio.gerenciamento.Fachada;
 
 public class TesteUser {
 
 	public static void main(String[] args) {
 
+		Fachada f = Fachada.getInstance();
 		
 		try {
+			f.conexao("root", "");
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
+		Usuario user = new Usuario(11010987, "12", "Aleatorio");
+		Calendar b = Calendar.getInstance();
+		b.set(2018, 8, 04);
+		Almoco a = new Almoco(b);
 		try {
+			f.insereUsuario(user);
+			f.login(user.getCpf(), user.getPassword());
+			f.adicionaAlmoco(a);
+			f.adicionaUsuarioDietaAlmoco(user, b);
+			//f.removeUsuario(user.getCpf());
 			//user.setCpf(11010);
 			//user.setPassword("123");
+			//f.atualizaUsuario(user);
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
 		try {
+			f.logout();
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}

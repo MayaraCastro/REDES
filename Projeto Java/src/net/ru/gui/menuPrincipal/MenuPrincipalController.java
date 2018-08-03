cd6cd477b40be5264c4c63686d08487bf206d84c
Mayara Castro <maaycastro@hotmail.com>
2018-08-03 20:38:32 -0300

    path

--- Projeto Java/src/net/ru/gui/menuPrincipal/MenuPrincipalController.java
+++ Projeto Java/src/net/ru/gui/menuPrincipal/MenuPrincipalController.java
@@ -3,15 +3,52 @@ import net.ru.gui.*;
 import net.ru.negocio.*;
 import com.gluonhq.charm.glisten.control.CardPane;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 
 public class MenuPrincipalController {
 
     @FXML
     private CardPane<?> lista_dias_da_semana;
-    
-    public void pegar() {
-    	System.out.println("iii");
-    	System.out.println(this.lista_dias_da_semana.getOnMouseClicked());
-    	}
+
+    @FXML
+    private Label segunda;
+
+    @FXML
+    private Label terca;
+
+    @FXML
+    private Label quarta;
+
+    @FXML
+    private Label quinta;
+
+    @FXML
+    private Label sexta;
+
+    @FXML
+    void quarta(MouseEvent event) {
+
+    }
+
+    @FXML
+    void quinta(MouseEvent event) {
+
+    }
+
+    @FXML
+    void segunda(MouseEvent event) {
+
+    }
+
+    @FXML
+    void sexta(MouseEvent event) {
+
+    }
+
+    @FXML
+    void terca(MouseEvent event) {
+
+    }
 
 }

1a04976416264d76d6890818b1f9f4a778bbb0a4
Wilder Santos Carvalho <wcarvalho98@gmail.com>
2018-08-03 20:35:48 -0300

    Usuário não pode se cadastrar duas vezes no mesmo cardápio

--- Projeto Java/src/net/ru/negocio/gerenciamento/GerenciamentoCardapio.java
+++ Projeto Java/src/net/ru/negocio/gerenciamento/GerenciamentoCardapio.java
@@ -31,26 +31,42 @@ public class GerenciamentoCardapio {
 		}	
 	}
 	
-	public void adicionaUsuarioVegetarianoAlmoco(Usuario user, Calendar dia) {
+	public void adicionaUsuarioVegetarianoAlmoco(Usuario user, Calendar dia) throws Exception {
 		Almoco a = (Almoco) p.busca(Almoco.class, dia);
+		for (Usuario aux : a.getVegetariano()) {
+			if (aux.getCpf() == user.getCpf())
+				throw new Exception("Usuário já cadastrado nesse cardápio");
+		}
 		a.getVegetariano().add(user);
 		p.atualiza(a);
 	}
 	
-	public void adicionaUsuarioDietaAlmoco(Usuario user, Calendar dia) {
+	public void adicionaUsuarioDietaAlmoco(Usuario user, Calendar dia) throws Exception {
 		Almoco a = (Almoco) p.busca(Almoco.class, dia);
+		for (Usuario aux : a.getDieta()) {
+			if (aux.getCpf() == user.getCpf())
+				throw new Exception("Usuário já cadastrado nesse cardápio");
+		}
 		a.getDieta().add(user);
 		p.atualiza(a);
 	}
 	
-	public void adicionaUsuarioVegetarianoJantar(Usuario user, Calendar dia) {
+	public void adicionaUsuarioVegetarianoJantar(Usuario user, Calendar dia) throws Exception {
 		Jantar j = (Jantar) p.busca(Jantar.class, dia);
+		for (Usuario aux : j.getVegetariano()) {
+			if (aux.getCpf() == user.getCpf())
+				throw new Exception("Usuário já cadastrado nesse cardápio");
+		}
 		j.getVegetariano().add(user);
 		p.atualiza(j);
 	}
 	
-	public void adicionaUsuarioDietaJantar(Usuario user, Calendar dia) {
+	public void adicionaUsuarioDietaJantar(Usuario user, Calendar dia) throws Exception {
 		Jantar j = (Jantar) p.busca(Jantar.class, dia);
+		for (Usuario aux : j.getDieta()) {
+			if (aux.getCpf() == user.getCpf())
+				throw new Exception("Usuário já cadastrado nesse cardápio");
+		}
 		j.getDieta().add(user);
 		p.atualiza(j);
 	}
